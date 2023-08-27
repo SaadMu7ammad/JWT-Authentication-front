b0c0838563f0dae6ca955219c70c0dfb85ff1773
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-08-28 00:58:24 +0300

    edit urls

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import All from './All/All';
 function App() {
   return (
     // <Provider store={store}>
-    <BrowserRouter >
+    <BrowserRouter basename="https://taskat.onrender.com">
+     
       <Routes>
         <Route index path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
